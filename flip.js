169f4bb9a3e60b050632728d61035f25852946a4
jonasriemersma <32481151+jonasriemersma@users.noreply.github.com>
2023-10-17 15:20:58 +0200

    Create flip.js

---
+++ flip.js
@@ -0,0 +1,52 @@
+const flipboxEl = document.querySelectorAll('.flip-box')[0];
+
+
+console.log(flipboxEl);
+
+function Flip(){
+    
+    if(flipboxEl.classList.contains("flipped")){
+    flipboxEl.classList.toggle("flipped");
+
+    }
+    else{
+    flipboxEl.classList.add("flipped");
+
+}
+}
+
+const flipboxEl1 = document.querySelectorAll('.flip-box')[1];
+
+
+console.log(flipboxEl1);
+
+function Flip1(){
+    
+    if(flipboxEl1.classList.contains("flipped")){
+    flipboxEl1.classList.toggle("flipped");
+
+    }
+    else{
+    flipboxEl1.classList.add("flipped");
+
+}
+}
+
+const flipboxEl2 = document.querySelectorAll('.flip-box')[2];
+
+
+console.log(flipboxEl2);
+
+function Flip2(){
+    
+    if(flipboxEl2.classList.contains("flipped")){
+    flipboxEl2.classList.toggle("flipped");
+
+    }
+    else{
+    flipboxEl2.classList.add("flipped");
+
+}
+}
+
+
