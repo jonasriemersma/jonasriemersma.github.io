8a20a140a63fcc7d5587cfdc1847df9d3430d376
jonasriemersma <32481151+jonasriemersma@users.noreply.github.com>
2024-02-14 14:32:41 +0100

    Update flip.js

--- flip.js
+++ flip.js
@@ -24,19 +24,23 @@ console.log(dragDuration)
 }); */
 
 
-function Flip(){
-    if(flipboxEl.classList.contains("flipped")){
-    flipboxEl.classList.toggle("flipped");
-    
+function Flip() {
+    if (flipboxEl.classList.contains("flipped")) {
+        flipboxEl.classList.toggle("flipped");
+
 
     }
-    else{
-    flipboxEl.classList.add("flipped");
-    flipboxEl1.classList.remove("flipped");
-    flipboxEl2.classList.remove("flipped");
-    flipboxEl3.classList.remove("flipped");
-
-}
+    else {
+        flipboxEl.classList.add("flipped");
+        flipboxEl1.classList.remove("flipped");
+        flipboxEl2.classList.remove("flipped");
+        flipboxEl3.classList.remove("flipped");
+        flipboxEl4.classList.remove("flipped");
+        flipboxEl5.classList.remove("flipped");
+        flipboxEl6.classList.remove("flipped");
+        flipboxEl7.classList.remove("flipped");
+        flipboxEl8.classList.remove("flipped");
+    }
 }
 
 const flipboxEl1 = document.querySelectorAll('.flip-box')[1];
@@ -44,19 +48,23 @@ const flipboxEl1 = document.querySelectorAll('.flip-box')[1];
 
 console.log(flipboxEl1);
 
-function Flip1(){
-    
-    if(flipboxEl1.classList.contains("flipped")){
-    flipboxEl1.classList.toggle("flipped");
+function Flip1() {
+
+    if (flipboxEl1.classList.contains("flipped")) {
+        flipboxEl1.classList.toggle("flipped");
 
     }
-    else{
+    else {
         flipboxEl.classList.remove("flipped");
         flipboxEl1.classList.add("flipped");
         flipboxEl2.classList.remove("flipped");
         flipboxEl3.classList.remove("flipped");
-
-}
+        flipboxEl4.classList.remove("flipped");
+        flipboxEl5.classList.remove("flipped");
+        flipboxEl6.classList.remove("flipped");
+        flipboxEl7.classList.remove("flipped");
+        flipboxEl8.classList.remove("flipped");
+    }
 }
 
 const flipboxEl2 = document.querySelectorAll('.flip-box')[2];
@@ -64,20 +72,24 @@ const flipboxEl2 = document.querySelectorAll('.flip-box')[2];
 
 console.log(flipboxEl2);
 
-function Flip2(){
-    
-    if(flipboxEl2.classList.contains("flipped")){
-    flipboxEl2.classList.toggle("flipped");
+function Flip2() {
+
+    if (flipboxEl2.classList.contains("flipped")) {
+        flipboxEl2.classList.toggle("flipped");
 
     }
-    else{
+    else {
         flipboxEl.classList.remove("flipped");
         flipboxEl1.classList.remove("flipped");
         flipboxEl2.classList.add("flipped");
         flipboxEl3.classList.remove("flipped");
-    
+        flipboxEl4.classList.remove("flipped");
+        flipboxEl5.classList.remove("flipped");
+        flipboxEl6.classList.remove("flipped");
+        flipboxEl7.classList.remove("flipped");
+        flipboxEl8.classList.remove("flipped");
 
-}
+    }
 }
 
 const flipboxEl3 = document.querySelectorAll('.flip-box')[3];
@@ -85,17 +97,144 @@ const flipboxEl3 = document.querySelectorAll('.flip-box')[3];
 
 console.log(flipboxEl3);
 
-function Flip3(){
-    
-    if(flipboxEl3.classList.contains("flipped")){
-    flipboxEl3.classList.toggle("flipped");
+function Flip3() {
+
+    if (flipboxEl3.classList.contains("flipped")) {
+        flipboxEl3.classList.toggle("flipped");
 
     }
-    else{
-    flipboxEl.classList.remove("flipped");
-    flipboxEl1.classList.remove("flipped");
-    flipboxEl2.classList.remove("flipped");
-    flipboxEl3.classList.add("flipped");
+    else {
+        flipboxEl.classList.remove("flipped");
+        flipboxEl1.classList.remove("flipped");
+        flipboxEl2.classList.remove("flipped");
+        flipboxEl3.classList.add("flipped");
+        flipboxEl4.classList.remove("flipped");
+        flipboxEl5.classList.remove("flipped");
+        flipboxEl6.classList.remove("flipped");
+        flipboxEl7.classList.remove("flipped");
+        flipboxEl8.classList.remove("flipped");
+    }
+}
 
+const flipboxEl4 = document.querySelectorAll('.flip-box')[4];
+
+console.log(flipboxEl4);
+
+function Flip4() {
+
+    if (flipboxEl4.classList.contains("flipped")) {
+        flipboxEl4.classList.toggle("flipped");
+
+    }
+    else {
+        flipboxEl.classList.remove("flipped");
+        flipboxEl1.classList.remove("flipped");
+        flipboxEl2.classList.remove("flipped");
+        flipboxEl3.classList.remove("flipped");
+        flipboxEl4.classList.add("flipped");
+        flipboxEl5.classList.remove("flipped");
+        flipboxEl6.classList.remove("flipped");
+        flipboxEl7.classList.remove("flipped");
+        flipboxEl8.classList.remove("flipped");
+
+    }
 }
+
+const flipboxEl5 = document.querySelectorAll('.flip-box')[5];
+
+console.log(flipboxEl5);
+
+function Flip5() {
+
+    if (flipboxEl5.classList.contains("flipped")) {
+        flipboxEl5.classList.toggle("flipped");
+
+    }
+    else {
+        flipboxEl.classList.remove("flipped");
+        flipboxEl1.classList.remove("flipped");
+        flipboxEl2.classList.remove("flipped");
+        flipboxEl3.classList.remove("flipped");
+        flipboxEl4.classList.remove("flipped");
+        flipboxEl5.classList.add("flipped");
+        flipboxEl6.classList.remove("flipped");
+        flipboxEl7.classList.remove("flipped");
+        flipboxEl8.classList.remove("flipped");
+
+    }
 }
+
+const flipboxEl6 = document.querySelectorAll('.flip-box')[6];
+
+console.log(flipboxEl6);
+
+function Flip6() {
+
+    if (flipboxEl6.classList.contains("flipped")) {
+        flipboxEl6.classList.toggle("flipped");
+
+    }
+    else {
+        flipboxEl.classList.remove("flipped");
+        flipboxEl1.classList.remove("flipped");
+        flipboxEl2.classList.remove("flipped");
+        flipboxEl3.classList.remove("flipped");
+        flipboxEl4.classList.remove("flipped");
+        flipboxEl5.classList.remove("flipped");
+        flipboxEl6.classList.add("flipped");
+        flipboxEl7.classList.remove("flipped");
+        flipboxEl8.classList.remove("flipped");
+
+    }
+}
+
+
+const flipboxEl7 = document.querySelectorAll('.flip-box')[7];
+
+console.log(flipboxEl7);
+
+function Flip7() {
+
+    if (flipboxEl7.classList.contains("flipped")) {
+        flipboxEl7.classList.toggle("flipped");
+
+    }
+    else {
+        flipboxEl.classList.remove("flipped");
+        flipboxEl1.classList.remove("flipped");
+        flipboxEl2.classList.remove("flipped");
+        flipboxEl3.classList.remove("flipped");
+        flipboxEl4.classList.remove("flipped");
+        flipboxEl5.classList.remove("flipped");
+        flipboxEl6.classList.remove("flipped");
+        flipboxEl7.classList.add("flipped");
+        flipboxEl8.classList.remove("flipped");
+
+    }
+}
+
+
+const flipboxEl8 = document.querySelectorAll('.flip-box')[8];
+
+console.log(flipboxEl8);
+
+function Flip8() {
+
+    if (flipboxEl8.classList.contains("flipped")) {
+        flipboxEl8.classList.toggle("flipped");
+
+    }
+    else {
+        flipboxEl.classList.remove("flipped");
+        flipboxEl1.classList.remove("flipped");
+        flipboxEl2.classList.remove("flipped");
+        flipboxEl3.classList.remove("flipped");
+        flipboxEl4.classList.remove("flipped");
+        flipboxEl5.classList.remove("flipped");
+        flipboxEl6.classList.remove("flipped");
+        flipboxEl7.classList.remove("flipped");
+        flipboxEl8.classList.add("flipped");
+
+    }
+}
+
